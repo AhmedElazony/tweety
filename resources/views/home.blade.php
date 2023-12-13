c20463a58a0edb2d6925cf9ff0eb1a454c7801ae
AhmedElazony <ahmedazony2@gmail.com>
2023-12-13 13:07:49 +0200

    some configs

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h1>
+            <img
+                src="{{ asset('images/logo.svg') }}"
+                alt="Tweety"
+            />
+        </h1>
+    </x-slot>
+    
+</x-app-layout>
