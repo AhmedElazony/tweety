7b51f57eb67aab13331fb9924bca06abab51b077
AhmedElazony <ahmedazony2@gmail.com>
2023-12-24 10:11:37 +0200

    make tweet and refactor user models

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +46,26 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function avatar(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => 'https://i.pravatar.cc/40?u=' . $this->email
+        );
+    }
+
+    public function tweets(): HasMany
+    {
+        return $this->hasMany(Tweet::class);
+    }
+
+    public function follows(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function timeline()
+    {
+        return Tweet::where('user_id', $this->id)->latest()->get();
+    }
 }
