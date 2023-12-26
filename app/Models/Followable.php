a1ffb74f1adaf5d71ab3206d0db74c67040bcf05
AhmedElazony <ahmedazony2@gmail.com>
2023-12-26 21:22:16 +0200

    make follow system login

---
+++ app/Models/Followable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+trait Followable
+{
+    // following relationship.
+    public function following(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id')->withTimestamps();
+    }
+
+    // followers relationship.
+    public function followers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'follows', 'following_user_id', 'user_id')->withTimestamps();
+    }
+
+    // make auth user follow other users.
+    public function follow(User $user): Model
+    {
+        return $this->following()->save($user);
+    }
+
+    public function unfollow(User $user)
+    {
+        return $this->following()->detach($user);
+    }
+
+    // check if the auth user is following the passed user or not.
+    public function isFollowing(User $user): bool
+    {
+        // this will make a load on the performance if the 'following' collection has a lot of records.
+        // return $this->following->contains($user);
+
+        return $this->following()
+            ->where('following_user_id', $user->id)
+            ->exists();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FollowsController;
 use App\Http\Controllers\TweetController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -16,19 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [TweetController::class, 'index'])->name('home');
+    Route::post('/tweets', [TweetController::class, 'store'])->name('tweet.store');
+
+    Route::post('/profiles/{user:name}/follow', [FollowsController::class, 'store'])->name('follow.store');
+    Route::delete('/profiles/{user:name}/follow', [FollowsController::class, 'destroy'])->name('follow.destroy');
 });
 
-Route::get('/home', [HomeController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('home');
-
-Route::post('/tweets', [TweetController::class, 'store']);
-
 Route::middleware('auth')->group(function () {
+    Route::get('/profiles/{user:name}', [ProfileController::class, 'show'])->name('profile.show');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 require __DIR__.'/auth.php';
