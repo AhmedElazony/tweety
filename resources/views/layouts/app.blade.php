b0ae252018c91295f980516d865f35613a19f542
AhmedElazony <ahmedazony2@gmail.com>
2024-01-09 20:37:14 +0200

    edit deployment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-f2a72f25.css')  }}">
 </head>
 
 <body class="font-sans antialiased">
@@ -63,5 +64,5 @@
         </section>
     </div>
 </body>
-
+<script src="{{ asset('build/assets/app-b1941ff8.js') }}"></script>
 </html>
