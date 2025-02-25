3981411cbfac7a77a8e986ec84bc7b95d470ea7b
AhmedElazony <ahmedazony2@gmail.com>
2025-02-25 23:41:00 +0200

    Fix

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -106,5 +106,5 @@ self.addEventListener("push", function (event) {
 
 self.addEventListener("notificationclick", function (event) {
     event.notification.close();
-    event.waitUntil(clients.openWindow("/"));
+    event.waitUntil(clients.openWindow("/chats"));
 });
